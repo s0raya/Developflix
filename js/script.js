44cb91d27fe9c61b30b0808f24209f7198597d10
s0raya <soraya.alcobet@gmail.com>
2023-11-13 23:21:57 +0100

    Ejercicio terminado

--- js/script.js
+++ js/script.js
@@ -1,2 +1,15 @@
 import peliculas from './peliculas.js'
 
+
+
+peliculas.forEach(pelicula => {
+    let img = `<img src="https://image.tmdb.org/t/p/w500${pelicula.poster_path}" alt="${pelicula.title}"></img>`;
+    let genIds = pelicula.genre_ids;
+    genIds.forEach(number => {
+        if (document.querySelector('#genero-'+number)) {
+            document.querySelector('#genero-'+number).innerHTML += `<figure>${img}<figcaption>${pelicula.title}</figcaption></figure>`;
+        }
+    });
+});
+
+
